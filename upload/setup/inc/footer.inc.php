868ea13248c309c3ad6126dd4a7ce764985fe4f8
Milad Kaveh <MK@TUKAIT.ORG>
2018-12-29 16:35:59 +0330

    os

---
+++ upload/setup/inc/footer.inc.php
@@ -0,0 +1,12 @@
+
+            <div class="clear"></div>
+        </div> <!-- content -->
+    </div> <!-- wizard -->
+    <div id="footer" class="centered">Copyright &copy; 2013 <a target="_blank" href="http://osticket.com">osTicket.com</a></div>
+
+    <script type="text/javascript" src="../js/jquery-1.11.2.min.js?e321982"></script>
+    <script type="text/javascript" src="../js/jstz.min.js?e321982"></script>
+    <script type="text/javascript" src="js/setup.js?e321982"></script>
+    <script type="text/javascript" src="js/tips.js?e321982"></script>
+</body>
+</html>
